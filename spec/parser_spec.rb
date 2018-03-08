654b63919bec9172ed56c3a14a2f8a7eeded88e6
Sam Pikesley <sam.pikesley@gmail.com>
2018-03-08 11:32:59 +0000

    Parse realistic file

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -17,5 +17,18 @@ module Keepasser
         expect(parser[2].group).to eq 'Bluth Company'
       end
     end
+
+    context 'parse a more complex file' do
+      parser = Parser.new 'spec/fixtures/multiple-groups.txt'
+
+      it 'has entries' do
+        expect(parser.length).to eq 4
+      end
+
+      it 'has correct groups' do
+        expect(parser[0].group).to eq 'Bluth Company'
+        expect(parser[3].group).to eq 'Sitwell Enterprises'
+      end
+    end
   end
 end
